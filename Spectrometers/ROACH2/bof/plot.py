2bfa4b3352691de17053e708b50ae97714e12cbd
Franco Curotto <francocurotto@gmail.com>
2019-12-18 16:30:30 -0300

    cleaning prior calandigital branch

--- Spectrometers/ROACH2/bof/plot.py
+++
@@ -1,8 +0,0 @@
-import json
-import matplotlib.pyplot as plt
-
-with open("frequency_response spec2in_4096ch_1080mhz.bof 2019-09-06 12:08:04.json") as json_file:
-    data = json.load(json_file)
-
-plt.plot(data['freqs'], data['frequency_response'][1])
-plt.show()
