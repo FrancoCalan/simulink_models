4c4bb94dadfba61adb27b5ef54322adaa8d5b9e7
Franco Curotto <francocurotto@gmail.com>
2020-01-03 12:10:23 -0300

    fix scripts

--- Digital_Sideband_Separation/ROACH1/bof/mini_plot_snapshot.py
+++ Digital_Sideband_Separation/ROACH1/bof/mini_plot_snapshot.py
@@ -12,8 +12,10 @@ from matplotlib.animation import FuncAnimation
 # communication parameters
 roach_ip = '192.168.1.11'
 roach_port = 7147
-#boffile = 'corr2in_1024ch_500mhz.bof'
-boffile = 'dss_1024ch_500mhz.bof'
+boffile = 'corr2in_1024ch_500mhz.bof'
+#boffile = 'dss_1024ch_500mhz.bof'
+#boffile = 'snap2in.bof'
+#boffile = 'spec2in_4096ch_500mhz.bof'
 program_bof = True
 
 # model parameters
--- Digital_Sideband_Separation/ROACH1/bof/mini_plot_spectra.py
+++ Digital_Sideband_Separation/ROACH1/bof/mini_plot_spectra.py
@@ -12,7 +12,8 @@ from matplotlib.animation import FuncAnimation
 # communication parameters
 roach_ip = '192.168.1.11'
 roach_port = 7147
-boffile = 'corr2in_1024ch_500mhz.bof'
+#boffile = 'corr2in_1024ch_500mhz.bof'
+boffile = 'dss_1024ch_500mhz.bof'
 program_bof = True
 
 # model parameters
@@ -23,8 +24,8 @@ cnt_rst_reg = 'cnt_rst'
 bram_addr_width = 8  # bits
 bram_word_width = 64 # bits
 pow_data_type   = '>u8'
-bram_a2    = ['dout_a2_0', 'dout_a2_1', 'dout_a2_2', 'dout_a2_3']
-bram_b2    = ['dout_b2_0', 'dout_b2_1', 'dout_b2_2', 'dout_b2_3']
+bram_a2    = ['a2_0', 'a2_1', 'a2_2', 'a2_3']
+bram_b2    = ['b2_0', 'b2_1', 'b2_2', 'b2_3']
 
 # experiment parameters
 acc_len = 2**16
@@ -32,7 +33,7 @@ acc_len = 2**16
 # derivative parameters
 nbrams    = len(bram_a2)
 nchannels = 2**bram_addr_width * nbrams
-freqs     = np.linspace(0, adc_freq, n_channels, endpoint=False)
+freqs     = np.linspace(0, adc_freq, nchannels, endpoint=False)
 dBFS      = 6.02*adc_bits + 1.76 + 10*np.log10(nchannels)
 specbrams_list = [bram_a2, bram_b2]
 
@@ -41,7 +42,7 @@ specbrams_list = [bram_a2, bram_b2]
 ##########################
 def main():
     print("Setting up plotting and data saving elements...")
-    fig, lines = create_figure()
+    fig, lines = create_figure(2, adc_freq, dBFS)
     print("done")
 
     print("Initializing ROACH communication...")
@@ -67,10 +68,6 @@ def main():
     #####################
     # Start measurement #
     #####################
-    print("Setting up plotting and data saving elements...")
-    fig, lines = create_figure(2, adc_freq, dBFS)
-    print("done")
-
     print("Setting accumulation register...")
     roach.write_int(acc_len_reg, acc_len)
     time.sleep(1)
@@ -85,7 +82,7 @@ def main():
         for line, specbrams in zip(lines, specbrams_list):
             # get spectral data
             specdata = read_interleave_data(roach, specbrams, 
-                bram_addr_width, bram_word_dwidth, pow_data_type)
+                bram_addr_width, bram_word_width, pow_data_type)
             specdata = scale_and_dBFS_specdata(specdata, acc_len, 
                 adc_bits, nchannels)
             line.set_data(freqs, specdata)
