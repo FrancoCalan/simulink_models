a65e4b7ea39777591bf412be2db58ec42d4e64dd
Franco Curotto <francocurotto@gmail.com>
2020-01-09 15:48:28 -0300

    finish calibration script

---
+++ Snapshots/ROACH2/bof/snap2in_calibrate_adc5g.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+calibrate_adc5g.py \
+    --ip         192.168.1.12 \
+    --bof        snap2in.bof.gz \
+    --genip      192.168.1.34 \
+    --genfreq    10 \
+    --genpow     -3 \
+    --zdok0snaps adcsnap0 \
+    --zdok1snaps adcsnap1 \
+    --do_mmcm \
+    `#--do_ogp` \
+    `#--do_inl` \
+    --load_ogp \
+    --load_inl \
+    --plot_snap \
+    --plot_spec \
+    --nsamples 200 \
+    --bandwidth 1080 \
+    --loaddir adc5gcal\ 2020-01-09\ 15:31:42
