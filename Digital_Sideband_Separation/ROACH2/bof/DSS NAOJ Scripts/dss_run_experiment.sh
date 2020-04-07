583ae4a258b6a13d13725e3ab950d2d9f4fde26c
Franco Curotto <francocurotto@gmail.com>
2020-04-07 18:18:29 -0400

    clean after experiments

--- Digital_Sideband_Separation/ROACH2/bof/DSS NAOJ Scripts/dss_run_experiment.sh
+++ Digital_Sideband_Separation/ROACH2/bof/DSS NAOJ Scripts/dss_run_experiment.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-#./initialize_raoch.sh
-#./calibrate_adc5g.sh
-#./synchronize_adc5g.py
-#./dss_hotcold_multilo.py
-#./send_telegram.py "DSS test started."
 ./dss_calibrate_multilo.py
-#./send_telegram.py "Finished with calibration."
 ./dss_compute_srr_multilo.py
-#./send_telegram.py "Test finished!"
